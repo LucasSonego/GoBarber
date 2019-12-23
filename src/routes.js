8b5f7811346e5fddc35c1e3c70dc134e69aa97cb
Lucas Sônego <lucassonego@ufpr.br>
2019-12-23 00:03:49 -0200

    feat: middleware checagem de autenticação

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,17 @@ const routes = new Router();
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
+import authMiddleware from "./app/middlewares/auth";
+
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put("/teste", (req, res) => {
+  console.log(req.userId);
+
+  return res.json();
+});
 
 export default routes;
