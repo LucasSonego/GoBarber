d9d1b346827bfa0a45c153c7efa08314cc5c9cc7
Lucas Sônego <sonego_lucas@outlook.com>
2019-12-25 21:35:44 -0200

    feat: Upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
@@ -13,5 +16,7 @@ routes.post("/sessions", SessionController.store);
 routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
+
+routes.post("/files", upload.single("file"), (req, res) => res.json());
 
 export default routes;
