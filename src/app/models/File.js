ccb55d825d1c569cf14d2ca2424b8db748073f36
Lucas Sônego <sonego_lucas@outlook.com>
2019-12-26 01:09:56 -0200

    feat: avatar_id em user, criada relação entre usuario e avatar (file)
    Criada tabela para "files", que salva o path e um id dos arquivos.
    Criada foreign key de avatar_id na tabela "users".

---
+++ src/app/models/File.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from "sequelize";
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: Sequelize.STRING,
+        path: Sequelize.STRING
+      },
+      {
+        sequelize
+      }
+    );
+
+    return this;
+  }
+}
+
+export default File;
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const upload = multer(multerConfig);
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddleware from "./app/middlewares/auth";
 
@@ -17,6 +18,6 @@ routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
 
-routes.post("/files", upload.single("file"), (req, res) => res.json());
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
